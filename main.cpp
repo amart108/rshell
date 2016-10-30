3c8aefcc81a3f56d854daa1f7774f05969430da9
Antonio Martinez <amart108@ucr.edu>
2016-10-29 20:25:23 -0700

    routine save

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
-
+#include <sys/types.h>
 using namespace std;
 
 int main(int argc, char*argv[])
@@ -25,6 +25,11 @@ int main(int argc, char*argv[])
 	//cout << "checking display: " << endl;
 	A.cleanUp();
 	A.comment();
+	// this shows how to turn a string into a char*
+	string data = A.At(2); // the string here is echo
+	char *dat = new char[data.length() + 1]; // this turn the string to char*
+	strcpy(dat,data.c_str());
+	cout << dat << endl;
 	//A.display();	
 	//cout << "Done." << endl;
 	return 0;
