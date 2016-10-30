9828497521af2b179970b769ded34d72611a285a
Minwhan Oh <moh008@hammer.cs.ucr.edu>
2016-10-29 21:07:58 -0700

    Oct 29 b4 sleep

--- tree.cpp
+++ tree.cpp
@@ -1,65 +1,75 @@
-#include <iostreamr>
+#include <iostream>
 #include <vector>
 #include <string>
-#include <unistd>
+#include <unistd.h>
 
 using namespace std;
 
 class Base{
 public:
     Base(){};
-    virtual void execute() = 0;			//pure virtual execute parameter
-    virtual bool cmd_check()
+    virtual void execute(string command) = 0;			//pure virtual execute parameter
+    virtual bool cmd_check(string command) = 0;
 };
 
 class Connector: public Base{
 protected:
-    char* argv[];
-    bool left_bool, right_bool;
+    string command;
 public:
     Connector(): Base(){};
-    Connector(): Base(){
+    Connector(string command): Base(){
+	this->command = command;
     };
-    bool cmd_check(left, right){
-	if(left_bool == true){
-	    if(right_bool == true){
-		execute(argv);
+    Connector(Base* left, Base* right): Base(){ //c'tor overloading with 				
+	this->left = left;			//left(cmd+executable) + right(cmd + executable)
+	this->right = right;
+    };
+    bool cmd_check(){			
+			//... split string command into two part(cmd + executable) and check those are valid
+	if(left is one of commands)
+	{
+	    if(right is one of executables){
+		return true;
 	    }
 	    else{
-		    cout << "Error message"<<endl;
+		cout << "Input valid command"<<endl;
+		return false;
 	    }
+	}
 	else{
-	    cout<<"Error message"<<endl;
+	    cout<<"Input valid command"<<endl;
+	    return false;
 	}
     };
-// 	return true ,if cmd is true
-    
-    void execute(char* argv){
-	    execute(argv);
+
+    void execute(){			//execute definition
+	if(this->command->cmd_check() == true){
+	   //execute
+	}
     }
-// return true if cmd is excuted 
-    
-};
+};    
 
 class Semi: public Base{
 protected:
-    Base* left;
-    Base* right;
-    char* argv[];
+    Base* left;				//command on left(cmd+executable)
+    Base* right;			//command on right(cmd+executable)
+    string command_left, command_right;
 public:
-    Semi(): Base(){};
-    Semi(Base* left, Base* right): Base(){
-	this->left = left;
+    Semi(): Base(){};			//default c'tor
+    Semi(Base* left, Base* right, string command1, string command2): Base(){
+	this->left = left;		//c'tor with two parameter which is separated in cmd+executable
 	this->right = right;
+	this->command_left = command1;
+	this->command_right = command2;
     };
-	
-    bool cmd_exe_check(){		//check command is executed
-    if(true){
-	return true;
-
-    void execute(){			//both left and right will be executed
-	if(cmd_check() == true){	//
-	    argv[]->execute();		
+    bool cmd_check(){
+	this->command_left->cmd_check();
+	this->command_right->cmd_check();
+    }
+    bool cmd_exe_check(){
+	this->left->cmd
+    void execute(string command){			//both left and right will be executed no matter what
+	right->execute();		//if invalid cmd executed, error message will appear.
 	};
 };
 
@@ -69,17 +79,18 @@ protected:
     Base* right;
     
 public:
-    And(): Base(){};
-    And(): Base(){
+    And(): Base(){};			//default c'tor
+    And(Base* left, Base* right): Base(){
+	this->left = left;		//c'tor with two parameter which is separated in cmd+executable
+	this->right = right;
     };
-    void print_output(){
     
     };
     void execute(){
-	if(cmd_exe_check(left) == true){
-	    right->execute();	//true means executed(with valid command)
-	    			//use bool function return true or false if command is executed or not
-				//we need two bools(one bool per each command which can be overwritten
+	if(cmd_check(left) == true){
+	    right->execute();		//true means executed(with valid command)
+	else if(cmd_check(left) != true)//use bool function return true or false if command is executed or not
+	    return;			//we need two bools(one bool per each command which can be overwritten
 	}
     };
 };
@@ -88,25 +99,16 @@ protected:
     Base* left;
     Base* right;
 public:
-    Or(): Base(){};
-    Or(): Base(){
-    };
-
-    void print_output(){
-    
+    Or(): Base(){};			//default c'tor
+    Or(Base* left, Base* right): Base(){
+	this->left = left; 		//c'tor with two parameter which is separated in cmd+executable
+	this->right = right;
     };
     void execute(){
-	if(left == false){	//false means not executed(with invalid command)
-	right->execute();
-	}    
+	if(cmd_check(left) != true){	//false means not executed(with invalid command)
+	    right->execute();
+	}
+	else if(cmd_check(left) == true){
+	    return;
     };
 };
-
-class Command: Base{
-private:
-    string cmd;
-public:
-    Command(): Base(){};
-    Command(string cmd): Base(){
-	this->cmd = cmd;
-    };
