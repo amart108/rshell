93753acbf7de2ec1f9497774f33c8095e4693e81
Antonio Martinez <amart108@ucr.edu>
2016-11-02 15:58:53 -0700

    routine

--- Cmd.h
+++ Cmd.h
@@ -13,32 +13,37 @@ protected:
 	string cmdline;// Is the string where the command line is stored
 void cleanUp(){ // removes all spaces from the command line and removes everything at and after the # character appears.
 	string nline;	
-for(unsigned i = 0; i < cmdline.length(); i++)
-{
-		if(cmdline.at(i) == '#'){
+	for(unsigned i = 0; i < cmdline.length(); i++)
+	{
+		if(cmdline.at(i) == '#')
+		{
 		cmdline.erase(i,cmdline.length());
 		}   
-}
+	}
 
 	split();
 
 }
 
-void split(){ // splits the cmdline by the connector and commands and orangized which commands go to which connectors not complete yer
+void split()
+{ // splits the cmdline by the connector and commands and orangized which commands go to which connectors not complete yer
 Base* t;
 Base* t2;
 //Base* t3;
 //Base* t4;
 vector<int> cnctrs;
 //cout << "WHYYYYYYYYY" << endl;
-for(unsigned i = 0; i + 1 <  cmdline.length(); i++){
+for(unsigned i = 0; i + 1 <  cmdline.length(); i++)
+{
+	if(cmdline.at(i) == ';' || cmdline.at(i) == '&' || cmdline.at(i) == '|')	
+	{
 
-	if(cmdline.at(i) == ';' || cmdline.at(i) == '&' || cmdline.at(i) == '|'){
-
-		if(cmdline.at(i + 1) == '&'){
+		if(cmdline.at(i + 1) == '&')
+		{
 		cnctrs.push_back(i);
 		}
-		else if(cmdline.at(i + 1) == '|'){
+		else if(cmdline.at(i + 1) == '|')
+		{
 		cnctrs.push_back(i);
 		}
 		else{
@@ -59,104 +64,169 @@ int cnt = 0;
 
 if(cnctrs.size() == 0)
 {
-	//cout << "single commands" << endl;
+	cout << "single commands" << endl;
 	/*if(cmdline.find(" ") != string::npos){
 		left = cmdline.substr(0,cmdline.find(" "));  }
 	cmdline.erase(0,cmdline.find(" "));
 	right = cmdline;*/
 	start = new Command(cmdline);
-	//cout << "done with singles" << endl;
+	cout << "done with singles" << endl;
 
 }
-else{
-	cout << "4" << endl;
-	for(unsigned j = 0; j + 1 < cnctrs.size(); j++){
-	if(cnctrs.size() > 1) 
+else
+{
+	if(cnctrs.size() > 1)
 	{
-		if(cnctrs.at(j) != (cnctrs.at(j + 1) - 1))
+		cout << "4" << endl;
+		for(unsigned j = 0; j + 1 < cnctrs.size(); j++)
 		{
+			cout << "J: " << j << endl;
+			if(cnctrs.at(j) != (cnctrs.at(j + 1) - 1))
+			{
 			cout << "5" << endl;
-			if(cmdline.at(cnctrs.at(j)) == ';'){
-			left = cmdline.substr(0, cnctrs.at(j));
-			cout << "6" << endl;  }
-			 // When semicolon in the first connector in a multi-command case
-			else{
-			left = cmdline.substr(0, cnctrs.at(j) - 1);}
-			right = cmdline.substr(cnctrs.at(j) + 1, cnctrs.at(j + 1) - cnctrs.at(j) - 1);
-			right.erase(0,1);
-			if(right.at(right.length() - 1) == ' '){
-			right.resize(right.length() - 1);}
-		
-			if(cmdline.at(cnctrs.at(j)) == ';'){
-		
-			connector = "semi"; }
-			if(cmdline.at(cnctrs.at(j)) == '&'){
-		
-			connector = "and";  }
-			if(cmdline.at(cnctrs.at(j)) == '|'){
-		
-			connector = "or"; }
-//			cout  << "makeing connectors" << endl;
+				if(cmdline.at(cnctrs.at(j)) == ';')
+				{
+					left = cmdline.substr(0, cnctrs.at(j));
+				} // When semicolon in the first connector in a multi-command case
+				else
+				{
+					left = cmdline.substr(0, cnctrs.at(j) - 1);
+				}
+				right = cmdline.substr(cnctrs.at(j) + 1, cnctrs.at(j + 1) - cnctrs.at(j) - 1);
+				right.erase(0,1);
+				if(right.at(right.length() - 1) == ' ')
+				{
+					right.resize(right.length() - 1);
+				}
+				if(cmdline.at(cnctrs.at(j)) == ';')
+				{
+						connector = "semi"; 
+				}
+				if(cmdline.at(cnctrs.at(j)) == '&')
+				{
+					connector = "and";  
+				}
+				if(cmdline.at(cnctrs.at(j)) == '|')
+				{
+					connector = "or"; 
+				}
+//				cout  << "makeing connectors" << endl;
 //			cout << "problems" << endl;
-			t2 = new Command(right);
-			cnt += 1;
-			cout << connector << endl;
-			if(cnt == 1){
-			t = new Command(left);
-				if(connector == "semi"){
-				start =  new Semi(t,t2);
+				t2 = new Command(right);
+					cnt += 1;
+//			cout << connector << endl;
+				t = new Command(left);
+				cout << "6" << endl;
+				if(j == 0)
+				{cout << "7" << endl;
+					if(connector == "semi")
+					{
+						start =  new Semi(t,t2);
+					}
+					else if(connector == "and")
+					{
+						start =  new And(t,t2);
+					}
+					else
+					{
+						start = new Or(t,t2);
+					}
+				
 				}
-				else if(connector == "and"){
-				start =  new And(t,t2);
-				}
-				else{
-				start = new Or(t,t2);
-				}
+				else
+				{
+					cout << "8" << endl;
+					cout << "OThers; " << endl;	
+					if(connector == "semi")
+					{
+						start = new Semi(start,t2);
+					}
+					else if(connector == "and")
+					{
+						start = new And(start,t2);
+					}	
+					else 
+					{
+						start = new Or(start,t2);
+					}		
+			
+			
+				}	
+
 			}
-			else{
-				if(connector == "semi"){
-				start = new Semi(start,t2);
+			else
+			{
+				cout << "9" << endl;
+
+				if(cmdline.at(cnctrs.at(j + 1)) == '&')
+				{
+					cout << "10" << endl;
+					right = cmdline.substr(cnctrs.at(j) + 1);
+					cout << right << endl;
+					//, cnctrs.at(j + 1) - cnctrs.at(j) - 1);	
+					right.erase(0,1);
+					if(right.at(right.length() - 1) == ' ')
+					{
+						right.resize(right.length() - 1);
+					}
+					connector == "and";
+					start = new And(start,t2);				
+				}	
+				else
+				{
+					right = cmdline.substr(cnctrs.at(j) + 1, cnctrs.at(j + 1) - cnctrs.at(j) - 1);
+                                        right.erase(0,1);
+                                        if(right.at(right.length() - 1) == ' ')
+                                        {
+                                                right.resize(right.length() - 1);
+                                        }
+					connector == "Or";
+					start = new Or(start, t2);
 				}
-				else if(connector == "and"){
-				start = new And(start,t2);
-				}
-				else {
-				start = new Or(start,t2);
-				}
+				cout << "j: " << j << endl;
+				cout << "j + 1: " << j + 1 << endl;
+			
+
 			}
+		}	
 	
-		}
-	}
-}	
+	}	
 //		cout << "7" << endl;
-		cnt += 1;
-		if(cmdline.at(cnctrs.at(cnctrs.size() - 1)) == ';'){
+	cnt += 1;
+	if(cmdline.at(cnctrs.at(cnctrs.size() - 1)) == ';')
+	{
 		connector = "semi";
 		left = cmdline.substr(0, (cnctrs.at(cnctrs.size() - 1)));
 		right = cmdline.substr(cnctrs.at(cnctrs.size() - 1));
 		right.erase(0,2);
 		
-		cout << connector << endl;
-		cout << left << endl;
-		cout << right << endl;
+			//cout << connector << endl;
+			//cout << left << endl;
+		//cout << right << endl;
 		t2 = new Command(right);
 		t = new Command(left);
 		start = new Semi(t, t2);
-		}
-		if(cmdline.at(cnctrs.at(cnctrs.size() -1)) == '&'){
+	}
+	if(cmdline.at(cnctrs.at(cnctrs.size() -1)) == '&')
+	{
 		connector = "and";
-		//cout << "AND " << endl;
-		//cout << "size: " << cnctrs.size() << endl;
-		if(cnctrs.size() < 2) {
-		//cout << "special case " << endl;
-		left = cmdline.substr(0, (cnctrs.at(cnctrs.size() - 1)));}
-		else{
+			//cout << "AND " << endl;
+				//cout << "size: " << cnctrs.size() << endl;
+		if(cnctrs.size() < 2) 
+		{
+			//cout << "special case " << endl;
+			left = cmdline.substr(0, (cnctrs.at(cnctrs.size() - 1)));
+		}
+		else
+		{
 		//cout << "normal" << endl;
-		left = cmdline.substr(0, (cnctrs.at(cnctrs.size() - 1)) - 1); }
+			left = cmdline.substr(0, (cnctrs.at(cnctrs.size() - 1)) - 1); 
+		}
 		right = cmdline.substr(cnctrs.at(cnctrs.size() - 1) + 1); // if right = & then user must enter another command 
 		right.erase(0,1);
 		
-		if(right == "&"){
+		if(right == "&")
+		{
 			right = ""; 
 			cout << "> ";
 			getline(cin, right); // need to work when enters nothing and presses enter
@@ -167,17 +237,23 @@ else{
 		t2 = new Command(right);
 		t = new Command(left);
 		start = new And(t, t2);
-		} // also make sure the both and and or do not remove the last char if they are they only ops with nothing after	
-		if(cmdline.at(cnctrs.at(cnctrs.size() -1)) == '|'){
+	} // also make sure the both and and or do not remove the last char if they are they only ops with nothing after	
+	if(cmdline.at(cnctrs.at(cnctrs.size() -1)) == '|')
+	{
 		connector = "or";
-		if(cnctrs.size() < 2){
-		left = cmdline.substr(0, (cnctrs.at(cnctrs.size() - 1)));}
-		else {
-		left = cmdline.substr(0, (cnctrs.at(cnctrs.size() - 1)) - 1); }
+		if(cnctrs.size() < 2)
+		{
+			left = cmdline.substr(0, (cnctrs.at(cnctrs.size() - 1)));
+		}
+		else 
+		{
+			left = cmdline.substr(0, (cnctrs.at(cnctrs.size() - 1)) - 1); 
+		}
 		right = cmdline.substr(cnctrs.at(cnctrs.size() - 1) + 1);// if right = | then user must enter another command or something
 		right.erase(0,1);
-		
-		if(right == "|"){
+			
+		if(right == "|")
+		{
 			right = "";
 			cout << "> ";
 			getline(cin, right);
@@ -188,12 +264,11 @@ else{
 		//cout << connector << endl;
 		//cout << left << endl;
 		//cout << right << endl;
-		}	
+	}			
 		//cout << "Cnt: " << cnt << endl;
-	}
 }
-
-;
+cout << "done with splits" << endl;
+};
 
 
 
--- tree.h
+++ tree.h
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <cstring>
 #include <stdlib.h>
+//#include <array>
 //#include <errno.h>
 
 //#include "CMD.h"
@@ -28,30 +29,42 @@ protected:
 
 public:
 	Command(string cm1): cm(cm1) {};
-	int execute(){
+	int execute() // executes a sing commamnd like ls, ls -a , echo hello, and so on
+	{
 	//cout << "start of the execution" << endl;	
 	
-	string t = "";
-	string t2 = "";
-	for(unsigned i = 0; i < cm.length(); i++)
-	{
-		if(cm.at(i) == ' ')
+		string t = "";
+		string t2 = "";
+		cout << "Cm:" << cm << endl;
+		for(unsigned i = 0; i < cm.length(); i++)
 		{
-			t = cm.substr(0,i);
-			cm.erase(0,i + 1);
-			t2 = cm;
+			if(cm.at(i) == ' ')
+			{
+				t = cm.substr(0,i);
+				cm.erase(0,i + 1);
+				t2 = cm;
+			//	t2 = t2 +"a"i;
+				if(t2.at(t2.length() - 1) == ' ')
+				{
+					t2.erase(t2.length() -1,1);
+				}
+			}
+			else
+			{
+				t = cm;
+			}
 		}
-	}
-	//cout << "Split the string cm into: " << t << " and " << t2 << endl;
-	
-	char* d1 = new char[t.length() + 1];
-	cout  << d1 << endl;
-	strcpy(d1, t.c_str());
-	cout << d1 << " and ";
-	char* d2 = new char[t2.length() + 1];
-	strcpy(d2, t2.c_str());
-	cout << d2 << endl;
-	char* arg[] = { d1, d2, NULL};	
+	cout << "Split the string cm into: " << t << " and" << t2 << endl;
+		char* d1 = new char[t.length() + 1];
+//cout  << d1 << endl;
+		strcpy(d1, t.c_str());
+		//cout << d1 << " and ";
+		char* d2 = new char[t2.length() + 1];
+		strcpy(d2, t2.c_str());
+		char* arg[] = { d1, d2, NULL};	
+			
+		
+		//cout << "Checking arg: "  << arg[0] << " and " << arg[1] << endl;
 	//cout << "Put into the char* arg" << endl;	
 	
 	// do fork and execvp and return when execvp does 
@@ -59,7 +72,7 @@ public:
 		if(pid < 0)
 		{
 			cout << "Fork is not operating.";
-			return -1;
+			exit(1);
 		}
 		else if (pid > 0)
 		{
@@ -71,7 +84,7 @@ public:
 			}
 			else if(*sit == 0)
 			{
-				return 0;
+				return 1;
 			}
 			else
 			{
@@ -83,57 +96,57 @@ public:
 			if(execvp(arg[0], arg) < 0)
 			{
 				cout << "The command failed to execute." << endl;
-				return -1;
+				exit(1);
 			}	
-			//delete [] arg;
-//			delete [] d1;
-//			delete [] d2;
-
+			exit(0);
+		
 		}
-		//delete [] arg;
-	
 		return 0;
+	
+		
 	}
 
 };
 
-class Semi: public Base{
+class Semi: public Base{ // this class happends when there is a ";" connector
 protected:
     Base* left;				//command on left(cmd+executable)
     Base* right;			//command on right(cmd+executable)
-//    string command_left, command_right;
+
 public:
 	Semi(Base* l, Base* r): left(l), right(r) {};
-	int execute()
+	int execute() // executes both and left commands
 	{	
 		cout << "Semi" << endl;
 		this->left->execute();
-		return this->right->execute();
+		this->right->execute();
+		return 1;
 	}
 };
 
-class And: public Base{
+class And: public Base{ // this is created when there is a && between two connectors
 protected:
     Base* left;
     Base* right;
     
 public:
-    And(Base* l, Base* r): left(l), right(r) {};
-	int execute()
+	And(Base* l, Base* r): left(l), right(r) {};
+	int execute() // This checks if the left command executed properly is it did then it executes the right command if not it return -1;
 	{
-		cout << "And" << endl;
-		this->left->execute();
-		/*int result = 0;
-		cout << "And" << endl;
-		if(this->left->execute() >= 0) 
+//		cout << "And" << endl;
+		//this->left->execute();
+		//int result = 0;
+		cout << "Left" << endl;
+		if(this->left->execute() > 0) 
 		{
-			 
+			cout << "Right" << endl;	
+			return this->right->execute(); 
 			
 		}
-		return result;*/
-	
-		return 1;;
-
+		else
+		{
+			return -1;
+		}
 	}
 	
 };
@@ -142,20 +155,20 @@ protected:
     Base* left;
     Base* right;
 public:
-    Or(Base* l, Base* r): left(l), right(r) {};
-    int execute()
+Or(Base* l, Base* r): left(l), right(r) {};
+int execute()
+{
+//	cout << "Or" << endl;
+	//this->left->execute();
+	if(this->left->execute() < 0)
+	{	
+		return this->right->execute();
+	}
+	else
 	{
-		cout << "1Or" << endl;
-		this->left->execute();
-		/*if(this->left->execute() < 0)
-		{	
-			return this->right->execute();
-		}
-		else
-		{
-			return 1;
-		}*/
-		return 7;
+		return 1;
+	}
+		
 	}
 };
 
